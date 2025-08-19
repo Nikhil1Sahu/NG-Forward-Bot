890e57067ffd5fc9aae6361282819c668fbe1e45
Nikhil1Sahu <168706708+Nikhil1Sahu@users.noreply.github.com>
2025-08-20 04:58:08 +0530

    Update config.py
    first

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
-# bot developer @mr_jisshu
+# bot developer @OwnerOfNG
 from os import environ 
 
 class Config:
     
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    API_ID = environ.get("API_ID", "18466881")
+    API_HASH = environ.get("API_HASH", "8c8ca14ad8e416ce4e6ea717db7ec6af")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8434121689:AAETQNk2JSU3N_t68TRkW8nDFvDBLLGu3oE") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", "5565120414").split()]
+    BOT_SESSION = environ.get("BOT_SESSION", "NG_share_bot") 
 
-    PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
+    PICS = (environ.get('PICS', 'https://envs.sh/uxo.jpeg https://envs.sh/uxr.jpeg https://envs.sh/uxs.jpeg'))
     
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://nikhilsahu7j:dTQKfvo0jABOYKOu@cluster0.n2csgvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002731391701'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/network_of_kingdom") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
 
@@ -25,4 +25,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
