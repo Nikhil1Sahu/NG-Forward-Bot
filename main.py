a53237a86806e7d8789f47117fb554d5fb1a0fa9
Nikhil1Sahu <168706708+Nikhil1Sahu@users.noreply.github.com>
2025-08-21 01:42:02 +0530

    Update main.py
    semi final try

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# bot developer @mr_jisshu
+# bot developer @OwnerofNG
 from bot import Bot
+from keep_alive import keep_alive
 
+# start the tiny Flask server (for Render port binding)
+keep_alive()
+
+# start your Telegram bot
 app = Bot()
 app.run()
